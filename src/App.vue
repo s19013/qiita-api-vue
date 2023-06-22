08ac83321f2a381296129d87ce980ae981538ca5
知念英弥 <s19013@std.it-college.ac.jp>
2023-06-23 00:09:01 +0900

    入力したキーワードで検索できるようにした

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,28 @@
 import axios from 'axios';
 import { ref,reactive,onMounted,nextTick } from 'vue';
 import ArticleComponent from './components/ArticleComponent.vue';
+import InputComponent from './components/InputComponent.vue';
+
+// コンポーネントたち
+const Input =ref(null)
+
+
 
 const articles = ref([]);
-
+const InputHistory = reactive({
+  page:1,
+  per_page:10,
+  keyword:null
+})
 
 /** qiitaの記事取ってくる */
-const getArticle = async() => {
+const getArticle = async({page,per_page,keyword}) => {
+  console.log(keyword);
   await axios.get('https://qiita.com/api/v2/items',{
     params:{
-      page:1,
-      per_page:10,
-      query:null,
+      page:page,
+      per_page:per_page,
+      query:keyword,
     }
   })
   /** @param {res} api通信で取ってきたデータたち */
@@ -25,13 +36,25 @@ const getArticle = async() => {
   })
 }
 
-// onMounted(() => { 
-//   getArticle()
-// });
+const search = () =>{
+  getArticle({
+    page:1,
+    per_page:10,
+    keyword:Input.value.passKeyword()
+  })
+}
+
+onMounted(() => { 
+  // getArticle()
+});
 </script>
 
 <template>
   <main>
+    <div class="search">
+      <InputComponent ref="Input" :original="InputHistory.keyword"/>
+      <button @click="search">検索</button>
+    </div>
     <template v-for="(article,index) in articles" :key="index">
       <ArticleComponent
         :article="article"
@@ -42,6 +65,6 @@ const getArticle = async() => {
 
 <style scoped lang="scss">
 .main{
-  width: 100%;
+
 }
 </style>
