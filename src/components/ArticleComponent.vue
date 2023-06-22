7f15aaf6aeefb0e560b69a3bb4cf2ba3951b88b4
知念英弥 <s19013@std.it-college.ac.jp>
2023-06-22 22:05:28 +0900

    記事の情報を表示するコンポーネント

---
+++ src/components/ArticleComponent.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref,reactive } from 'vue';
+import { defineProps } from 'vue';
+
+// 親から受け取るデータ
+const props = defineProps(['article'])
+
+</script>
+
+<template>
+    <div class="article">
+        <a :href="props.article.url">
+            <h2>{{ props.article.title }}</h2>
+        </a>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.article{
+    border: 1px dashed #000000;
+    margin: 0.5rem;
+    padding:0.5rem;
+
+}
+h4{margin: 0.5rem 0 ;}
+</style>
