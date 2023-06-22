5e2c446b9b66c5bbba268de4a65eb0eabc1353f6
知念英弥 <s19013@std.it-college.ac.jp>
2023-06-23 00:08:36 +0900

    キーワードを入力するコンポーネント

---
+++ src/components/InputComponent.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {ref,defineProps,watch } from 'vue';
+
+const props = defineProps({
+    original:{
+        type:String,
+        default:null,
+    }
+})
+const keyword = ref(props.original)
+// 親が子の情報を受け取る
+const passKeyword = () => {
+    return keyword.value
+}
+
+// propsの値が変化したらkeywordを更新する
+watch(origin,(newValue,oldValue) => {
+  keyword.value = newValue
+})
+
+
+defineExpose({
+    passKeyword
+});
+</script>
+
+<template>
+    <input type="text" v-model="keyword"/>
+</template>
+
+<style lang="scss" scope>
+</style>
