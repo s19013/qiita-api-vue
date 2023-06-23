ff043a5d756100fab83850abc9d2c15efa561e74
知念英弥 <s19013@std.it-college.ac.jp>
2023-06-23 16:29:56 +0900

    セレクターを作った

---
+++ src/components/SelectorComponent.vue
@@ -0,0 +1,44 @@
+<script setup>
+import {ref,defineProps,watch } from 'vue';
+
+const selected = ref(props.original)
+
+const props = defineProps({
+    original:{
+        type:[ String, Number],
+        required: true
+    },
+    options:{
+        type:Array,
+        required: true
+    }
+})
+
+// 親が子の情報を受け取る
+const passSelected = () => {
+    return selected.value
+}
+
+// propsの値が変化したらselectedを更新する
+watch(() => props.original,(newValue,oldValue) => {
+  selected.value = newValue
+})
+
+defineExpose({
+    passSelected
+});
+</script>
+
+<template>
+    <div class="selector">
+        <select v-model="selected">
+            <option v-for="(option,index) in options" :key="index" :value="option">
+                {{ option }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
